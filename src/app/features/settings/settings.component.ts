f8bf9cd359f96f54ee7142242888f67e409d030d
PC-PAC\davide <dpacquola@gmail.com>
2024-11-18 16:46:59 +0100

    CMD

--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { EditorComponent } from './components/editor.component';
 import { PreviewComponent } from './components/preview.component';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
   imports: [
     EditorComponent,
-    PreviewComponent
+    PreviewComponent,
+    NgOptimizedImage
   ],
   template: `
     <h1 class="text-3xl text-center my-6">Settings</h1>
@@ -28,6 +30,28 @@ import { PreviewComponent } from './components/preview.component';
      sia sulla navbar che in editor e preview -->
     <button (click)="doNothing()">do nothing</button>
     <input type="text" (keydown)="doNothing()">
+
+    <br>
+
+    <div class="page">
+      <h1>Demo CDN + ngSRC</h1>
+
+      <img ngSrc="../../../assets/images/js.png" alt=""
+           width="400" height="400"
+           ngSrcset="600w, 1024w, 1400w"
+           sizes="(max-width: 600px) 100vw, 50vw"
+           priority="high"
+      >
+
+      <div>Powered by</div>
+      <img
+        ngSrc="../../../assets/images/js.png?imgeng=w_200" alt=""
+        [width]="200"
+        [height]="70"
+      >
+
+    </div>
+
 
   `,
   styles: ``
