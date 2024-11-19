d9ee18969f452a6dbda5b471fbac542727761a81
PC-PAC\davide <dpacquola@gmail.com>
2024-11-19 10:43:30 +0100

    new test2

--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -36,7 +36,7 @@ import {NgOptimizedImage} from '@angular/common';
     <div class="page">
       <h1>Demo CDN + ngSRC  </h1>
 
-      <img src="../../../assets/images/img_big.JPG" alt=""
+      <img src="../../assets/images/img_big.JPG" alt=""
            width="400" height="400"
            ngSrcset="600w, 1024w, 1400w"
            sizes="(max-width: 600px) 100vw, 50vw"
@@ -45,7 +45,7 @@ import {NgOptimizedImage} from '@angular/common';
 
       <div>Powered by</div>
       <img
-        src="../../../assets/images/js.png?imgeng=w_200" alt=""
+        src="../../assets/images/js.png?imgeng=w_200" alt=""
         [width]="200"
         [height]="70"
       >
