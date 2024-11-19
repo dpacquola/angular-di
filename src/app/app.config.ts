78f7ad5bdc36e0e9d8fef4da22936f2f77bcea68
PC-PAC\davide <dpacquola@gmail.com>
2024-11-19 11:24:41 +0100

    test finale CDN

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { IMAGE_LOADER, ImageLoaderConfig } from '@angular/common';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+const host = 'https://gqctmza7.dev.cdn.imgeng.in';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: IMAGE_LOADER,
+      useValue: (config: ImageLoaderConfig) => {
+        console.log(config)
+        return config.width ?
+          `${host}/${config.src}?imgeng=${config.width}` :
+          `${host}/${config.src}`
+      }
+    }
+  ]
 };
--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -34,8 +34,17 @@ import {NgOptimizedImage} from '@angular/common';
     <br><br><br>
 
     <div class="page">
-      <h1>Demo CDN + ngSRC ssss </h1>
+      <h1>Demo CDN + ngSRC</h1>
 
+      1 IMG senza ottimizzazione CDN
+      <img src="assets/images/img_big.JPG" alt="">
+
+      <!--      immmagine con il PATH specificato che punta alla CDN-->
+      2 IMG con CDN con path esplicito
+      <img src="https://gqctmza7.dev.cdn.imgeng.in/assets/images/img_big.JPG" alt="">
+
+<!--      uso ngSrc per ottimizzare le immaginia, definito in Angular, e avendo modificato il app.config.ts punto ancora su CDN-->
+      3 Uso cgSrc con modifica path su app.config
       <img ngSrc="assets/images/img_big.JPG" alt=""
            width="400" height="400"
            ngSrcset="600w, 1024w, 1400w"
